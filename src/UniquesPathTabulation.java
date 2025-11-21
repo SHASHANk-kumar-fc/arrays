291cb09b21ef1d39e5940e716b8179180e1ed633
HP <shashankfc8@gmail.com>
2025-11-21 18:37:03 +0530

    Tabulation

---
+++ src/UniquesPathTabulation.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int dp[][]=new int [m][n];
+        dp[0][0]=1;
+        for(int i=0;i<m;i++){
+            int j=(i==0)?1:0;
+            while(j<n){
+                if(j-1>=0)
+                    dp[i][j]=dp[i][j-1];
+                if(i-1>=0)
+                    dp[i][j]+=dp[i-1][j];
+
+                j++;
+            }
+        }
+        return dp[m-1][n-1] ;
+
+
+    }
+
+}
