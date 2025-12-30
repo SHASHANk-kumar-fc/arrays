7641ccab3778d09d7005e40f7e84bd207aef33b6
HP <shashankfc8@gmail.com>
2025-12-30 18:58:25 +0530

    Greedy

---
+++ src/Leetcode_2366.java
@@ -0,0 +1,18 @@
+lass Solution {
+public long minimumReplacement(int[] nums) {
+    int prev=nums[nums.length-1];
+    long count=0;
+
+    for(int i=nums.length-2;i>=0;i--){
+        if(prev<nums[i]){
+            int k=(nums[i]+prev-1)/prev;
+            count+=k-1;
+            prev=nums[i]/k;
+        }else{
+            prev=nums[i];
+        }
+    }
+
+    return count;
+}
+}
